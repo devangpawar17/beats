7410b52f08b910b9cd97a6dbe4f59f682201be4b
devang pawar <80813053+devangpawar17@users.noreply.github.com>
2024-04-21 21:21:54 +0530

    Update dataScrapper.js

--- dataScrapper.js
+++ dataScrapper.js
@@ -6,7 +6,8 @@ const dataScrapper = async () => {
     try {
         // Launch a headful browser
         const browser = await puppeteer.launch({executablePath: '/usr/bin/chromium-browser',
-        args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+                                               headless: false});
 
         // Open a new page
         const page = await browser.newPage();
